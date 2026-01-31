f7ca78b6601929e06bb4150758df337ffeeef013
javiwar04 <javier.guerra294@gmail.com>
2026-01-31 00:39:58 -0600

    arreglando diseño

--- app/transferencias/page.tsx
+++ app/transferencias/page.tsx
@@ -114,9 +114,9 @@ export default function TransferenciasPage() {
   })
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex min-h-screen w-full bg-muted/40">
       <StaticSidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-64">
+      <div className="flex flex-1 flex-col sm:gap-4 sm:py-4">
         <Header />
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <div className="flex items-center gap-4">
